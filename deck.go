595701516f66727ccae0f0ef7014706b59f8b2bb
Fabian Ramirez <fabian.ramirez@gmail.com>
2020-06-16 21:16:23 -0400

    Feature: Cards aree mixed and displayed using receiver functions

---
+++ deck.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+type deck []string
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
+
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Spades", "Diamons", "Hearts", "Clubs"}
+	cardValues := []string{"One", "Two", "Three", "Four"}
+
+	for _, suite := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suite)
+		}
+	}
+	return cards
+}
